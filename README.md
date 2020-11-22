6ab2106e09d3840805c335d37b26efc83378d06e
pers0na2 <bloothering@rambler.ru>
2020-11-22 20:02:44 +0300

    Init

--- README.md
+++
@@ -1,2 +0,0 @@
-# QuarcCode
- A minimalistic code editor written in C++ using ImGui and OpenGL
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdexcept>
+
+#include "frontend/libraries/borderless/borderless.hpp"
+
+int CALLBACK wWinMain(HINSTANCE /*inst*/, HINSTANCE /*prev*/, LPWSTR /*cmd*/, int /*show*/) {
+	try {
+		BorderlessWindow window;
+	}
+	catch (const std::exception& e) {
+		::MessageBoxA(nullptr, e.what(), "Unhandled Exception", MB_OK | MB_ICONERROR);
+	}
+}
